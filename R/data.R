b0aefda185fb8a33e76877ceef4e84c978cab534
Alston <s07308@gmail.com>
2022-10-20 11:21:47 +0800

    edit the document of the example data

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Inotuzumab ozogamicin data
+#'
+#' Phase 3 clinical trial which compared the effects of inotuzumab ozogamicin (group 1) with standard chemotherapy (group 0)
+#' for treating of acute lymphoblastic leulemia. This dataset is obtained by re-digitizing the KM curves from the original medical paper.
+#' Kantarjian HM, DeAngelo DJ, Stelljes M, et al. Inotuzumab Ozogamicin versus Standard Therapy for Acute Lymphoblastic Leukemia. N Engl J Med. 2016;375(8):740-753.
+#'
+#'
+#' @format ## `Inotuzumab`
+#' A data frame with 1621 rows and 3 columns:
+#' \describe{
+#'   \item{surv.time}{the survival of each subject in the trial}
+#'   \item{event}{censoring indicator}
+#'   \item{arm}{treatment arm}
+#' }
+#' @source <https://www.nejm.org/doi/full/10.1056/nejmoa1509277>
+"Inotuzumab"
